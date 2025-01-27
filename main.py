a3b8f28b7006ef988005b532974e4c7b25e95c31
xnathanhd <xnathanhd@hotmail.co.uk>
2025-01-27 13:43:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from website import create_app
 
 app = create_app()
 
+
+@app.route('/')
+def home():
+    return "Hello, Railway!"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
